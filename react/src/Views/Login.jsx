7a1dbc6815452f0be14b6f96e460e07276e59a17
babak Mirhosseini <sbabakm@gmail.com>
2023-04-14 17:11:59 +0430

    install bootstrap and complete login and sign up page

--- react/src/Views/Login.jsx
+++ react/src/Views/Login.jsx
@@ -1,8 +1,16 @@
 function Login() {
     return (
-        <h2>
-            Login
-        </h2>
+        <form className="w-25 m-auto border p-5">
+            <div className="form-outline mb-4">
+                <label className="form-label">Name</label>
+                <input type="text" id="" className="form-control" />
+            </div> 
+            <div className="form-outline mb-4">
+                <label className="form-label">Password</label>
+                <input type="password" id="" className="form-control" />
+            </div>      
+            <button type="button" className="btn btn-primary btn-sm btn-block mb-4">Sign in</button>
+        </form>
     )
 }
 
--- react/src/Views/Signup.jsx
+++ react/src/Views/Signup.jsx
@@ -1,8 +1,24 @@
 function Signup() {
     return (
-        <h2>
-            Signup
-        </h2>
+        <form className="w-25 m-auto border p-5">
+            <div className="form-outline mb-4">
+                <label className="form-label">Name</label>
+                <input type="text" id="" className="form-control" />
+            </div> 
+            <div className="form-outline mb-4">
+                <label className="form-label">Email</label>
+                <input type="email" id="" className="form-control" />
+            </div> 
+            <div className="form-outline mb-4">
+                <label className="form-label">Password</label>
+                <input type="password" id="" className="form-control" />
+            </div>   
+            <div className="form-outline mb-4">
+                <label className="form-label">Repeat your password</label>
+                <input type="password" id="" className="form-control" />
+            </div>      
+            <button type="button" className="btn btn-primary btn-sm btn-block mb-4">Register</button>
+    </form>
     )
 }
 
