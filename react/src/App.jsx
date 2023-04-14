3b5754034555bf87d0302a39bb206f101777807a
babak Mirhosseini <sbabakm@gmail.com>
2023-04-14 15:39:32 +0430

    install react router dom , create routes , create views

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import {Routes, Route} from 'react-router-dom';
+
+//import views
+import Login from './Views/Login';
+import Signup from './Views/Signup';
+import Users from './Views/Users';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <Routes>
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='users' element={<Users />} />
+      </Routes>
+    </>
   )
 }
 
