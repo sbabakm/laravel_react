65101d6ea45f4419366557b50f0690f726d72aab
babak Mirhosseini <sbabakm@gmail.com>
2023-04-26 14:22:56 +0330

    add fonts to project

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import {BrowserRouter} from 'react-router-dom';
 import './../node_modules/bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
