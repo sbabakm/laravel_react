a0056d7a7161e3615f6332dd3d2b4ccacc78a4e0
babak Mirhosseini <sbabakm@gmail.com>
2023-04-28 01:06:08 +0430

    complete bdg items form

--- react/src/Views/CreateBdgItems.jsx
+++ react/src/Views/CreateBdgItems.jsx
@@ -7,14 +7,21 @@ function CreateBdgItems() {
 
     return (
         <form id="header-form" onSubmit={onSubmitItems} className="w-50 mx-auto mt-5 border p-5">
-            <div className="form-outline mb-4">
-                <label className="form-label">cost</label>
-                <input type="text" className="form-control" />
+            <div className="d-flex align-items-center">
+                <div className="form-outline mb-4 d-flex align-items-center">
+                    <label className="form-label me-2">cost</label>
+                    <input type="text" className="form-control me-2" />
+                </div>
+                <div className="form-outline mb-4 d-flex align-items-center">
+                    <label className="form-label me-2">month</label>
+                    <input type="text" className="form-control me-2" />
+                </div>
             </div>
-            <div className="form-outline mb-4">
-                <label className="form-label">month</label>
-                <input type="text" className="form-control" />
-            </div>
+            <button
+                className="btn btn-danger btn-sm btn-block mb-4 d-block"
+            >
+                add row
+            </button>
             <button
                 type="submit"
                 className="btn btn-primary btn-sm btn-block mb-4"
